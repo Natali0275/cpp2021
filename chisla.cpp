40daced7b43ca2d2fd3bcb4c6f04bdd7cace9f48
Natali0275 <101510804+Natali0275@users.noreply.github.com>
2022-03-13 17:42:04 +0300

    chisla.cpp

---
+++ chisla.cpp
@@ -0,0 +1,23 @@
+#include <std>
+ 
+ int main ()
+ {
+   int a,b;
+   float rt, g;
+   char tx,tc;
+   a=10;
+   b=2;
+   rt = 2.2;
+   g = 3.5;
+   tx = '4';
+   tc = 'мама';
+   c = a/b;
+   rg = rt*g;
+   t = tx + tc;
+   print ("Значение с: %d", c);
+   print ("Значение rg: %f", rg);
+   print ("Значение t: %c", t);
+   
+ return 0;
+ 
+ }
